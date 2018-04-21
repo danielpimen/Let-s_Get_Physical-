be06e7f203d8285d22c665433801b88751969f27
Daniel P <danielpimen92@gmail.com>
2018-04-21 16:30:47 -0400

    incorporated routes

--- models/User.js
+++ models/User.js
@@ -2,3 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //Schema
+const UserSchema = new Schema({
+  name: {
+    first: {type: String, required: true},
+    last: {type: String, required: true},
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  myWorkOuts: [{type: Number, ref: 'User'}],
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const passport = require('passport');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const users = require('./routes/users');
 // Body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -23,9 +25,7 @@ if (process.env.NODE_ENV === 'production') {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
+app.use('/users', users);
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
