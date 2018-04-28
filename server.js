7c03dd6a4aceac7d95a6d8db1843cd83159d03d8
Daniel P <danielpimen92@gmail.com>
2018-04-28 12:53:55 -0400

    updates

--- server.js
+++ server.js
@@ -19,9 +19,11 @@ mongoose
   .catch(err => console.log(err));
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/public/index.html'));
+});
 
 // Send every request to the React app
 // Define any API routes before this runs
